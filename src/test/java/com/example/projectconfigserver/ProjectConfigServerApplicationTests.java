e44b9058774ed5b9dca718000179a13e94b19f03
ShashinduMalshan <shasidumalshan9579@gmail.com>
2026-08-27 16:28:49 +0530

    Initial commit for Project-Config-Server

---
+++ src/test/java/com/example/projectconfigserver/ProjectConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
